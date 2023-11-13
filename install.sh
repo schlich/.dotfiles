9094ebe626b0ccd5f1e82e659a90615151e028d9
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-11-13 03:06:37 -0600

    add p10k source script to zsh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 cp -a powerlevel10k/. ~/
 
