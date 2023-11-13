13eae90818ecdf46bc147280e80a910fbae1172b
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-11-13 03:21:22 -0600

    fix cp syntax

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
-cp -a powerlevel10k/. ~
+cp -a powerlevel10k/. ~/
 
 cp .zshrc ~
